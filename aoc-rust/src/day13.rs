7f63e28d753e8a421bc897a26a3b3524084ed0d1
Tyson Liddell <tysonliddell@hotmail.com>
2023-12-14 00:00:35 +0000

    Refactor day 13.

--- aoc-rust/src/day13.rs
+++ aoc-rust/src/day13.rs
@@ -16,7 +16,7 @@ fn str_to_bitstring(s: &str) -> u32 {
 
 struct Board {
     rows: Vec<u32>, // vector of row bit strings.
-    cols: Vec<u32>, // vector of column bit strings.
+    width: usize,
 }
 
 impl From<&str> for Board {
@@ -25,19 +25,9 @@ impl From<&str> for Board {
         let width = rows[0].len();
 
         let row_bitstrings = rows.into_iter().map(str_to_bitstring).collect_vec();
-        let col_bitstrings = (0..width)
-            .rev() // puzzle numbers columns msb to lsb (first col is msb)
-            .map(|c| {
-                row_bitstrings
-                    .iter()
-                    .map(|r| if r & (1 << c) == 0 { 0 } else { 1 })
-                    .fold(0, |acc, v| (acc << 1) | v)
-            })
-            .collect_vec();
-
         Self {
             rows: row_bitstrings,
-            cols: col_bitstrings,
+            width,
         }
     }
 }
@@ -47,70 +37,71 @@ impl Display for Board {
         let s = self
             .rows
             .iter()
-            .map(|r| format!("{:0width$b}", r, width = self.cols.len()))
+            .map(|r| format!("{:0width$b}", r, width = self.width))
             .join("\n");
         write!(f, "{}", s)
     }
 }
 
 impl Board {
-    fn index_of_symmetry(&self) -> usize {
-        if let Some(val) = get_line_of_symmetry(&self.rows) {
-            100 * val
-        } else {
-            get_line_of_symmetry(&self.cols).expect("Should find a line of symmetry!")
-        }
-    }
-
-    fn index_of_smudged_symmetry(&self) -> usize {
-        if let Some(val) = get_smudged_line_of_symmetry(&self.rows) {
-            100 * val
-        } else {
-            get_smudged_line_of_symmetry(&self.cols)
-                .expect("Should find a line of smudged symmetry!")
-        }
-    }
-}
-
-fn get_line_of_symmetry(bitstrings: &Vec<u32>) -> Option<usize> {
-    let is_line_of_symmetry = |line_num| {
-        let rows_before = bitstrings.iter().rev().skip(bitstrings.len() - line_num);
-        let rows_after = bitstrings.iter().skip(line_num);
-        for (row_before, row_after) in rows_before.zip(rows_after) {
-            if row_before != row_after {
-                return false;
-            }
-        }
-        true
-    };
-
-    (1..bitstrings.len()).find(|&line| is_line_of_symmetry(line))
-}
-
-fn get_smudged_line_of_symmetry(bitstrings: &Vec<u32>) -> Option<usize> {
-    let is_smudged_line_of_symmetry = |line_num| {
-        let rows_before = bitstrings.iter().rev().skip(bitstrings.len() - line_num);
-        let rows_after = bitstrings.iter().skip(line_num);
-
-        let mut seen_smudged_bit = false;
-        for (row_before, row_after) in rows_before.zip(rows_after) {
-            let num_bits_different = (row_before ^ row_after).count_ones();
-
-            #[allow(clippy::comparison_chain)]
-            if num_bits_different > 1 {
-                return false;
-            } else if num_bits_different == 1 {
-                if seen_smudged_bit {
+    fn row_of_symmetry(&self) -> Option<usize> {
+        let rows = &self.rows;
+        let is_line_of_symmetry = |line_num| {
+            let rows_before = rows.iter().rev().skip(rows.len() - line_num);
+            let rows_after = rows.iter().skip(line_num);
+            for (row_before, row_after) in rows_before.zip(rows_after) {
+                if row_before != row_after {
                     return false;
                 }
-                seen_smudged_bit = true;
             }
+            true
+        };
+
+        (1..rows.len()).find(|&line| is_line_of_symmetry(line))
+    }
+
+    fn row_of_smudged_symmetry(&self) -> Option<usize> {
+        let rows = &self.rows;
+        let is_smudged_line_of_symmetry = |line_num| {
+            let rows_before = rows.iter().rev().skip(rows.len() - line_num);
+            let rows_after = rows.iter().skip(line_num);
+
+            let mut seen_smudged_bit = false;
+            for (row_before, row_after) in rows_before.zip(rows_after) {
+                let num_bits_different = (row_before ^ row_after).count_ones();
+
+                #[allow(clippy::comparison_chain)]
+                if num_bits_different > 1 {
+                    return false;
+                } else if num_bits_different == 1 {
+                    if seen_smudged_bit {
+                        return false;
+                    }
+                    seen_smudged_bit = true;
+                }
+            }
+
+            seen_smudged_bit
+        };
+
+        (1..rows.len()).find(|&line| is_smudged_line_of_symmetry(line))
+    }
+
+    fn rotate(&self) -> Self {
+        let col_bitstrings = (0..self.width)
+            .rev() // puzzle numbers columns msb to lsb (first col is msb)
+            .map(|c| {
+                self.rows
+                    .iter()
+                    .map(|r| if r & (1 << c) == 0 { 0 } else { 1 })
+                    .fold(0, |acc, v| (acc << 1) | v)
+            })
+            .collect_vec();
+        Self {
+            rows: col_bitstrings,
+            width: self.rows.len(),
         }
-
-        seen_smudged_bit
-    };
-
-    (1..bitstrings.len()).find(|&line| is_smudged_line_of_symmetry(line))
+    }
 }
 
 fn parse_boards() -> Vec<Board> {
@@ -121,15 +112,30 @@ fn parse_boards() -> Vec<Board> {
 
 fn part1() {
     let boards = parse_boards();
-    let total = boards.iter().map(|b| b.index_of_symmetry()).sum::<usize>();
+    let mut total = 0;
+    for board in boards {
+        if let Some(row) = board.row_of_symmetry() {
+            total += 100 * row;
+        } else if let Some(row) = board.rotate().row_of_symmetry() {
+            total += row;
+        } else {
+            panic!("No line of symmetry found!");
+        }
+    }
     println!("{}", total);
 }
 
 fn part2() {
     let boards = parse_boards();
-    let total = boards
-        .iter()
-        .map(|b| b.index_of_smudged_symmetry())
-        .sum::<usize>();
+    let mut total = 0;
+    for board in boards {
+        if let Some(row) = board.row_of_smudged_symmetry() {
+            total += 100 * row;
+        } else if let Some(row) = board.rotate().row_of_smudged_symmetry() {
+            total += row;
+        } else {
+            panic!("No line of smudged symmetry found!");
+        }
+    }
     println!("{}", total);
 }
