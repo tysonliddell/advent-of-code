09e5f544ffd0c5c4b99c6f7b6565359b73ada80c
Tyson Liddell <tysonliddell@hotmail.com>
2024-12-03 06:35:10 +0000

    Add day 3, 2024.

--- 2024/aoc-rust-2024/src/lib.rs
+++ 2024/aoc-rust-2024/src/lib.rs
@@ -1,5 +1,6 @@
 mod day1;
 mod day2;
+mod day3;
 mod helpers;
 mod io;
 
@@ -7,6 +8,7 @@ pub fn solution(day: u8, part: u8) -> String {
     let solution: Box<dyn Solution> = match day {
         1 => Box::new(day1::Day1),
         2 => Box::new(day2::Day2),
+        3 => Box::new(day3::Day3),
         _ => unimplemented!("Day {} is not implemented!", day),
     };
 
