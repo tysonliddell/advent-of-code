c078f243723e416b2f1fd339354e1f6586dda5f4
Tyson Liddell <tysonliddell@hotmail.com>
2024-12-19 14:36:14 +0000

    Make day 18 fast.

--- 2024/aoc-rust-2024/src/bin/day18.rs
+++ 2024/aoc-rust-2024/src/bin/day18.rs
@@ -1,6 +1,8 @@
 use std::collections::{HashSet, VecDeque};
 
 use aoc_rust_2024::io;
+use itertools::Itertools;
+use partitions::{partition_vec, PartitionVec};
 
 type BytePosition = (usize, usize);
 
@@ -89,7 +91,61 @@ fn part2_solution() -> (usize, usize) {
     panic!("No solution found!");
 }
 
+fn get_9_cell_square(pos: BytePosition, max_row: usize, max_col: usize) -> Vec<BytePosition> {
+    let (row, col) = (pos.0 as i32, pos.1 as i32);
+    let nbrs = (row - 1..=row + 1).cartesian_product(col - 1..=col + 1);
+    nbrs.into_iter()
+        .filter(|(row, col)| {
+            (0..=max_row as i32).contains(row) && (0..=max_col as i32).contains(col)
+        })
+        .map(|(row, col)| (row as usize, col as usize))
+        .collect()
+}
+
+// uses a union-find data structure
+fn part2_solution_fast() -> (usize, usize) {
+    let positions = parse_input();
+    let mut cells: PartitionVec<bool> = partition_vec![false; (MAX_MEM_ROW+1) * (MAX_MEM_COL+1)];
+
+    for byte_pos in positions {
+        let new_id = byte_pos.0 * (MAX_MEM_COL + 1) + byte_pos.1;
+        if cells[new_id] {
+            continue; // we've already added this falling byte, nothing to do
+        }
+
+        cells[new_id] = true;
+
+        // Merge cells that are touching the new position
+        for (row, col) in get_9_cell_square(byte_pos, MAX_MEM_ROW, MAX_MEM_COL) {
+            let id = row * (MAX_MEM_COL + 1) + col;
+            if cells[id] {
+                cells.union(new_id, id);
+            }
+        }
+
+        for region in cells.all_sets() {
+            let (mut min_row, mut max_row) = (MAX_MEM_ROW, 0);
+            let (mut min_col, mut max_col) = (MAX_MEM_COL, 0);
+
+            for (index, _) in region {
+                let row = index / (MAX_MEM_COL + 1);
+                let col = index % (MAX_MEM_COL + 1);
+                min_row = min_row.min(row);
+                max_row = max_row.max(row);
+                min_col = min_col.min(col);
+                max_col = max_col.max(col);
+            }
+            if min_row == 0 && max_row == MAX_MEM_ROW {
+                return byte_pos;
+            }
+        }
+    }
+
+    panic!("No solution found!");
+}
+
 fn main() {
     println!("{}", part1_solution());
-    println!("{:?}", part2_solution());
+    // println!("{:?}", part2_solution());
+    println!("{:?}", part2_solution_fast());
 }
