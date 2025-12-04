afa4f5cc09fe4b436cdf7f470e64f3f842d86a11
Tyson Liddell <tysonliddell@hotmail.com>
2025-12-04 10:13:43 +0000

    Create util function for grid neighbours.

--- 2025/aoc-2025-zig/day4.zig
+++ 2025/aoc-2025-zig/day4.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const util = @import("./util.zig");
 
 pub fn main() !void {
     const input = @embedFile("./puzzle_input/d4");
@@ -13,49 +14,13 @@ pub fn main() !void {
 }
 
 inline fn is_accessible(row: usize, col: usize, grid: [][]u8) bool {
-    const height = grid.len;
-    const width = grid[0].len;
     var count_rolls: u8 = 0;
-    if (row > 0) {
-        if (col > 0) {
-            if (grid[row - 1][col - 1] == '@') {
-                count_rolls += 1;
-            }
-        }
-        if (grid[row - 1][col] == '@') {
-            count_rolls += 1;
-        }
-        if (col < width - 1) {
-            if (grid[row - 1][col + 1] == '@') {
-                count_rolls += 1;
-            }
-        }
-    }
 
-    if (col > 0) {
-        if (grid[row][col - 1] == '@') {
+    const neighbours = util.grid_neighbours(row, col, grid.len, grid[0].len);
+    for (neighbours) |pos| {
+        const r, const c = pos;
+        if (grid[r][c] == '@') {
             count_rolls += 1;
-        }
-    }
-    if (col < width - 1) {
-        if (grid[row][col + 1] == '@') {
-            count_rolls += 1;
-        }
-    }
-
-    if (row < height - 1) {
-        if (col > 0) {
-            if (grid[row + 1][col - 1] == '@') {
-                count_rolls += 1;
-            }
-        }
-        if (grid[row + 1][col] == '@') {
-            count_rolls += 1;
-        }
-        if (col < width - 1) {
-            if (grid[row + 1][col + 1] == '@') {
-                count_rolls += 1;
-            }
         }
     }
 
@@ -147,4 +112,20 @@ test "testing part 2 with test input" {
     const grid = try parse_input(test_input);
     const result = solve_part_2(grid);
     try std.testing.expectEqual(43, result);
+}
+
+const test_input_small =
+    \\.@.
+    \\@@@
+    \\.@.
+;
+
+test "testing small input" {
+    const grid_p1 = try parse_input(test_input_small);
+    const p1 = solve_part_1(grid_p1);
+    try std.testing.expectEqual(4, p1);
+
+    const grid_p2 = try parse_input(test_input_small);
+    const p2 = solve_part_2(grid_p2);
+    try std.testing.expectEqual(5, p2);
 }
---
+++ 2025/aoc-2025-zig/util.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+
+pub inline fn int_cast(T: type, v: anytype) T {
+    return @intCast(v);
+}
+
+pub fn grid_neighbours(row: usize, col: usize, height: usize, width: usize) [][2]usize {
+    var neighbours: [8][2]usize = undefined;
+    var neighbours_count: usize = 0;
+    comptime var r_offset = -1;
+    inline while (r_offset <= 1) : (r_offset += 1) {
+        comptime var c_offset = -1;
+        inline while (c_offset <= 1) : (c_offset += 1) {
+            if (r_offset == 0 and c_offset == 0) {
+                continue;
+            }
+
+            const r: i64 = int_cast(i64, row) + r_offset;
+            const c: i64 = int_cast(i64, col) + c_offset;
+            if (r >= 0 and r < height and c >= 0 and c < width) {
+                neighbours[neighbours_count] = .{ int_cast(usize, r), int_cast(usize, c) };
+                neighbours_count += 1;
+            }
+        }
+    }
+    return neighbours[0..neighbours_count];
+}
