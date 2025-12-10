30a98b5786d0daa4d84a8a6eec91e4445c13848c
Tyson Liddell <tysonliddell@hotmail.com>
2025-12-10 22:33:57 +0000

    Add day 9, 2025.
    Solution for part 2 is currently too slow and does not produce a result. Need to optimise!

---
+++ 2025/aoc-2025-zig/day9.zig
@@ -0,0 +1,147 @@
+const std = @import("std");
+
+const MAX_NUM_POSITIONS = 500;
+const Position = struct { x: u32, y: u32 };
+
+const PUZZLE_INPUT = @embedFile("./puzzle_input/d9");
+pub fn main() !void {
+    var position_buf: [MAX_NUM_POSITIONS]Position = undefined;
+    const positions = try parse_input(PUZZLE_INPUT, &position_buf);
+    const p1 = solve_part_1(positions);
+    std.debug.print("{}\n", .{p1});
+    const p2 = solve_part_2(positions);
+    std.debug.print("{}\n", .{p2});
+}
+
+fn area(pos1: Position, pos2: Position) u64 {
+    const dx: u64 = @abs(@as(i64, @intCast(pos1.x)) - @as(i64, @intCast(pos2.x))) + 1;
+    const dy: u64 = @abs(@as(i64, @intCast(pos1.y)) - @as(i64, @intCast(pos2.y))) + 1;
+    return dx * dy;
+}
+
+fn solve_part_1(positions: []Position) u64 {
+    var max_area: u64 = 0;
+    for (positions, 0..) |pos1, pos_id| {
+        for (positions[pos_id + 1 ..]) |pos2| {
+            const curr_area = area(pos1, pos2);
+            if (curr_area > max_area) {
+                max_area = curr_area;
+            }
+        }
+    }
+    return max_area;
+}
+
+fn solve_part_2(positions: []Position) u64 {
+    var max_area: u64 = 0;
+    for (positions, 0..) |pos1, pos_id| {
+        for (positions[pos_id + 1 ..]) |pos2| {
+            const curr_area = area(pos1, pos2);
+            if (curr_area > max_area and is_rectangle_inside(pos1, pos2, positions)) {
+                max_area = curr_area;
+            }
+        }
+    }
+    return max_area;
+}
+
+fn is_rectangle_inside(pos1: Position, pos2: Position, red_tiles: []Position) bool {
+    const min_x, const max_x = if (pos1.x < pos2.x) .{ pos1.x, pos2.x } else .{ pos2.x, pos1.x };
+    const min_y, const max_y = if (pos1.y < pos2.y) .{ pos1.y, pos2.y } else .{ pos2.y, pos1.y };
+
+    for (min_x..max_x + 1) |x| {
+        for (min_y..max_y + 1) |y| {
+            if (!is_position_inside(.{ .x = @intCast(x), .y = @intCast(y) }, red_tiles)) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+//
+// ###########
+// #  p1     #
+// ######### #
+//    p2   # #
+// ######### #
+// #  p3     #
+// ###########
+// Since the curve forms a circuit, there must be an even total number of
+// horizontal lines. A given non-edge point lies in the interior if and only if
+// there are an odd number of horizontal edges above/below it.
+// p1,p3 above are interior points, and p3 is an exterior point.
+fn is_position_inside(pos: Position, red_tiles: []Position) bool {
+    var num_horiz_edge_above: usize = 0;
+    for (red_tiles[0 .. red_tiles.len - 1], red_tiles[1..]) |tile1, tile2| {
+        if (tile1.y != tile2.y) {
+            // this is a vertical edge
+            std.debug.assert(tile1.x == tile2.x);
+            const x = tile1.x;
+            const min, const max = if (tile1.y < tile2.y) .{ tile1.y, tile2.y } else .{ tile2.y, tile1.y };
+            if (pos.x == x and pos.y >= min and pos.y <= max) {
+                return true; // position lies in veritical edge
+            } else if (pos.x == x and max < pos.y) {
+                num_horiz_edge_above += 1; // hack to keep parity correct
+            }
+            continue; // skip vertical edge
+        }
+
+        // this is a horizontal edge
+        const y = tile1.y;
+        std.debug.assert(tile1.y == tile2.y);
+        const min, const max = if (tile1.x < tile2.x) .{ tile1.x, tile2.x } else .{ tile2.x, tile1.x };
+        if (y <= pos.y and pos.x >= min and pos.x <= max) {
+            if (pos.y == y) {
+                return true; // position lies in horizontal edge
+            }
+            num_horiz_edge_above += 1;
+        }
+    }
+
+    return num_horiz_edge_above % 2 == 1;
+}
+
+fn parse_input(comptime input: []const u8, big_enough_slice: []Position) ![]Position {
+    const trimmed_input = std.mem.trim(u8, input, &[_]u8{'\n'});
+    var line_it = std.mem.splitScalar(u8, trimmed_input, '\n');
+    var result = big_enough_slice;
+    result.len = 0;
+    while (line_it.next()) |line| {
+        var tokens = std.mem.splitScalar(u8, line, ',');
+        const x = try std.fmt.parseUnsigned(u32, tokens.next().?, 10);
+        const y = try std.fmt.parseUnsigned(u32, tokens.rest(), 10);
+        result.len += 1;
+        result[result.len - 1] = .{ .x = x, .y = y };
+    }
+
+    // add final wrap around point to make it easy to compute edges later
+    result.len += 1;
+    result[result.len - 1] = result[0];
+    return result;
+}
+
+const test_input =
+    \\7,1
+    \\11,1
+    \\11,7
+    \\9,7
+    \\9,5
+    \\2,5
+    \\2,3
+    \\7,3
+;
+
+test "testing part 1 with test input" {
+    var position_buf: [MAX_NUM_POSITIONS]Position = undefined;
+    const positions = try parse_input(test_input, &position_buf);
+    const result = solve_part_1(positions);
+    try std.testing.expectEqual(50, result);
+}
+
+test "testing part 2 with test input" {
+    var position_buf: [MAX_NUM_POSITIONS]Position = undefined;
+    const positions = try parse_input(test_input, &position_buf);
+    const result = solve_part_2(positions);
+    try std.testing.expectEqual(24, result);
+}
